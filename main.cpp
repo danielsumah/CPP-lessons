5e6d942b19f176ac316b24b04ed4a91cba014486
danielsumah <danielsumah@gmail.com>
2021-09-02 20:51:46 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Daniel";
+    int age = 12;
+
+    cout << "Hello world!" << endl;
+    cout << "My name is " << name << " and i am " << age << " years old" << endl;
+    return 0;
+}
