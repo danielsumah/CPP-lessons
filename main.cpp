5a9271ac1dadfaa86008d050716558bf939c5958
danielsumah <danielsumah@gmail.com>
2021-09-03 11:03:26 +0100

    simple while loops

--- main.cpp
+++ main.cpp
@@ -2,40 +2,14 @@
 
 using namespace std;
 
-string dayOfTheWeek(int dayNum)
-{
-    string dayName;
-
-    switch(dayNum){
-    case 0:
-        dayName = "Sunday";
-        break;
-    case 1:
-        dayName = "Monday";
-        break;
-    case 2:
-        dayName = "Tuesday";
-        break;
-    case 3:
-        dayName = "Wednesday";
-        break;
-    case 4:
-        dayName = "Thursday";
-        break;
-    case 5:
-        dayName = "Friday";
-        break;
-    case 6:
-        dayName = "Saturday";
-        break;
-    default:
-        dayName = "Invalid day Number";
-    }
-    return dayName;
-}
 
 int main()
 {
-    cout << dayOfTheWeek(1);
+    int num = 1;
+
+    while(num <=10){
+        cout << num << endl;
+        num++;
+    }
     return 0;
 }
