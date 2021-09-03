87db0942fec72642350bd8da18fd84e6cbaa1ac4
danielsumah <danielsumah@gmail.com>
2021-09-03 10:42:07 +0100

    calculator can perform + - * and /

--- main.cpp
+++ main.cpp
@@ -2,17 +2,32 @@
 
 using namespace std;
 
-int getMax(int n1, int n2, int n3){
-    if(n1 >= n2 && n1 >= n3){
-        return n1;
-    } else if (n2 >= n1 && n2 >= n3){
-        return n2;
-    } else {
-        return n3;
-    }
-}
+
 int main()
 {
-    cout << getMax(320,54,60);
-    return 0;
+    int n1, n2, result;
+    char op;
+
+    cout << "Enter your first number : ";
+    cin >> n1;
+
+    cout << "Enter your Second number : ";
+    cin >> n2;
+
+    cout << "Enter Operation : ";
+    cin >> op;
+
+    //cout << performCal(n1, n2, op);
+    if(op == '+'){
+        result = n1 + n2;
+    } else if (op == '-'){
+        result = n1 - n2;
+    } else if (op == '*'){
+        result = n1 * n2;
+    }else if (op == '/'){
+        result = n1 / n2;
+    }else{
+        cout << "Invalid Operator entered" << endl;
+    }
+    cout << result;
 }
