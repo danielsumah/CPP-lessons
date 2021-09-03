e16f2313691800845b1b335b7987bf3cd7e49ac4
danielsumah <danielsumah@gmail.com>
2021-09-03 07:16:27 +0100

    functions

--- main.cpp
+++ main.cpp
@@ -2,22 +2,11 @@
 
 using namespace std;
 
+double cubeT(double number){
+    return number * number * number;
+}
 int main()
 {
-    // arrays
-
-    int numbers[] = {5,2,7,4,5,2};
-    cout << numbers[1] << endl;
-
-    //modifying numbers in an array
-    numbers[2] = 20;
-    cout << numbers[2] << endl;
-
-    // you can tell how much elements goes into the array
-    int numbers2[5];
-    numbers2[0] = 4;
-    cout << numbers2[0];
-
-
+    cout << cubeT(4);
     return 0;
 }
