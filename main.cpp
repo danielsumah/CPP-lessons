62f00aff26e9b67dead16bc01fe708c2a0be4350
danielsumah <danielsumah@gmail.com>
2021-09-03 07:34:39 +0100

    if statements with condition checks

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 
 using namespace std;
 
-double cubeT(double number){
-    return number * number * number;
+int getMax(int n1, int n2, int n3){
+    if(n1 >= n2 && n1 >= n3){
+        return n1;
+    } else if (n2 >= n1 && n2 >= n3){
+        return n2;
+    } else {
+        return n3;
+    }
 }
 int main()
 {
-    cout << cubeT(4);
+    cout << getMax(320,54,60);
     return 0;
 }
